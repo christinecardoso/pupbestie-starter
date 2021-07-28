52d3e0b0a216743cbcbad31fe228481006cb545a
christinecardoso <christinesalafia@gmail.com>
2021-07-28 19:36:29 -0400

    feat: flatpickr node #add to scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import Alpine from 'alpinejs';
+import flatpickr from 'flatpickr';
 
 window.Alpine = Alpine;
 
@@ -17,3 +18,15 @@ if ('serviceWorker' in navigator && env === 'production') {
     }
   });
 }
+
+flatpickr('#flatpickr', {
+  mode: 'multiple',
+  // dateFormat: "Y-m-d",
+  // defaultDate: ["2021-07-20", "2021-07-23"],
+  enableTime: true,
+  altInput: true,
+  altFormat: 'F j @ H:i',
+  dateFormat: 'm-d-Y H:i',
+  minDate: 'today',
+  maxDate: new Date().fp_incr(90), // 14 days from now
+});
